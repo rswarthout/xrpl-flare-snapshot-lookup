0cf584c878b48269ea7492c0bf2f2529bb6c7d32
Robert Swarthout <rswarthout@gmail.com>
2020-12-20 18:17:06 -0500

    Inital build of the site.

---
+++ src/pages/Index.js
@@ -0,0 +1,215 @@
+import React from 'react';
+import axios from "axios";
+var moment = require('moment');
+
+export default class Index extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            lookingUpAddress: false,
+            loadingFailed: false,
+            xrplAddress: ""
+        };
+
+        this.getAddressDetails = this.getAddressDetails.bind(this);
+        this.handleAddressChange = this.handleAddressChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleAddressChange(event) {
+        this.setState({
+            xrplAddress: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+
+        if (this.state.lookingUpAddress) {
+            return;
+        }
+
+        this.setState({
+            lookingUpAddress: true,
+            loadingFailed: false,
+            addressDetails: null
+        });
+
+        this.getAddressDetails();
+    }
+
+    async getAddressDetails() {
+        var self = this;
+
+        let data = await axios
+            .get("https://api.towo.io/v2/spark/info/" + this.state.xrplAddress)
+            .then(function (response) {
+                return response;
+            })
+            .catch(function(error) {
+                self.setState({
+                    loadingFailed: true
+                });
+                console.log(error);
+            });
+
+        this.setState({
+            lookingUpAddress: false
+        });
+
+        if (data) {
+            this.setState({
+                addressDetails: data.data
+            });
+        }
+    }
+
+    render() {
+
+        let addressDetails = "";
+
+        if (this.state.loadingFailed) {
+            addressDetails = (
+                <div className="rounded-lg shadow bg-red-50 p-4">
+                    <div className="flex">
+                        <div className="flex-shrink-0">
+                            <svg className="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                        <div className="ml-3">
+                            <h3 className="text-sm font-medium text-red-800">
+                                Error loading the wallet address
+                            </h3>
+                            <div className="mt-2 text-sm text-red-700">
+                                <p>
+                                    The address provided above could not be loaded. Please review the address.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else if (this.state.lookingUpAddress === true) {
+            addressDetails = (
+                <div className="text-center">
+                    <svg className="animate-spin  inline-block -mt-1 mr-3 h-5 w-5 text-gray-900" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Loading address details
+                </div>
+            );
+        } else if (this.state.addressDetails) {
+
+            let snapshotDate = moment('2020-12-12T00:00:00Z', "YYYY-MM-DDTHH:mm:ssZ");
+
+            addressDetails = (
+                <div>
+                    <div className="bg-white shadow overflow-hidden rounded-lg">
+                        <div className="px-4 py-5 sm:px-6">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Account Information
+                            </h3>
+                            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                                The amounts shown below are calculated from the XRPL snapshot taken on <br /><span className="font-bold">{ snapshotDate.format('YYYY-MM-DD HH:mm:ss Z') }</span>.
+                            </p>
+                        </div>
+                        <div className="border-t border-gray-200">
+                            <dl>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                        XRPL Address
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 break-words">
+                                        { this.state.addressDetails.xrplAddress }
+                                    </dd>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                        XRP Balance
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        { this.state.addressDetails.xrpBalance }
+                                    </dd>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                        Flare Address
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 break-words">
+                                        { this.state.addressDetails.flareAddress }
+                                        <div className="text-xs text-gray-400">(If this is not the address you were expecting, data is still being synced from the snapshot.)</div>
+                                    </dd>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                        Spark Claim
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        { this.state.addressDetails.sparkClaim }
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                    <div className="mt-2 text-gray-400 text-sm text-center">The data shown above is provided by the <a href="https://towo.io/" className="underline">Towo Labs API</a>.</div>
+                </div>
+            );
+        }
+
+        return (
+            <div className="max-w-screen-xl mx-auto px-2 pb-6">
+
+                <div className="mt-10 md:mt-16 text-center space-x-5 md:space-x-20">
+                    <svg className="w-auto h-20 md:h-40 inline-block" viewBox="0 0 512 424">
+                        <path d="M437,0h74L357,152.48c-55.77,55.19-146.19,55.19-202,0L.94,0H75L192,115.83a91.11,91.11,0,0,0,127.91,0Z" />
+                        <path d="M74.05,424H0L155,270.58c55.77-55.19,146.19-55.19,202,0L512,424H438L320,307.23a91.11,91.11,0,0,0-127.91,0Z" />
+                    </svg>
+                    <div className="w-auto h-20 md:h-40 inline-block text-gray-900 text-5xl">+</div>
+                    <svg className="w-auto h-20 md:h-40 inline-block" viewBox="0 0 109.6 55.3" >
+                        <path fill="#353A69" d="M12.5 49c0 3.5-2.8 6.3-6.3 6.3C2.8 55.3 0 52.5 0 49s2.8-6.3 6.3-6.3c3.4.1 6.2 2.9 6.2 6.3z" />
+                        <path fill="#353A69" d="M0 33.3c0-.4 0-.8.1-1.1.1-1.2.4-2.3.9-3.4.9-1.9 2.3-3.4 4.1-4.4 1.6-.9 3.3-1.5 5.1-1.7 1-.1 2.1-.2 3.1-.2H37v.7c-.1 1.3-.4 2.6-.9 3.9-.9 1.9-2.4 3.5-4.4 4.5-1.5.8-3.2 1.3-4.9 1.6-1 .2-2.1.2-3.2.2H0z" />
+                        <path fill="#353A69" d="M56.6 0c0 .6 0 1.3-.1 1.9-.3 1.9-1.2 3.7-2.5 5.1-1.2 1.2-2.6 2.1-4.1 2.7-1.8.7-3.6 1-5.5 1.1H.8v-.2c0-1.5.3-2.9.9-4.2.9-2 2.3-3.5 4.2-4.5C7.4 1 9.2.5 10.9.2 12 .1 13.1 0 14.1 0h42.5z" />
+                        <path fill="#353A69" d="M80.8 54.6H78.4c0-.9 0-1.7-.1-2.6-.1.1-.2.2-.2.3-1.1 1.5-2.8 2.5-4.6 2.7-1.5.2-3-.1-4.3-.8-1.9-.9-3.2-2.6-3.7-4.6-.6-1.9-.4-4 .3-5.9.6-1.5 1.7-2.7 3.1-3.4 1.8-1 4-1.2 6-.6 1.4.4 2.6 1.2 3.4 2.4 0 0 .1.1.1.2v-.7c0-.4.1-.8.4-1.1.3-.5.8-.8 1.4-.8h.7l-.1 14.9zM73 52.7c.2 0 .6 0 .9-.1 1.3-.2 2.5-.9 3.3-2 1.1-1.5 1.4-3.5.8-5.3-.5-1.8-2-3.1-3.8-3.5-.8-.2-1.6-.2-2.4 0-1.9.3-3.5 1.6-4.1 3.5-.3.8-.4 1.6-.3 2.5.1.9.3 1.7.8 2.5 1.1 1.6 2.9 2.5 4.8 2.4z" />
+                        <path fill="#353A69" d="M96.7 48.1c0 .3.1.5.1.7.4 1.8 1.8 3.2 3.7 3.6 2 .5 4.1.1 5.8-1 .4-.3 1-.3 1.4 0 .2.1.3.3.5.4s.3.3.5.4l-.3.3c-.9.9-2.1 1.6-3.4 2-1.7.5-3.6.6-5.4.1-2.1-.5-3.8-2-4.7-3.9-1-2.2-1-4.7 0-6.9.7-1.5 1.8-2.7 3.2-3.5 1.9-1 4-1.3 6.1-.8 1.1.2 2.2.7 3 1.5 1.1.9 1.8 2.2 2.1 3.5.3 1.1.3 2.3.2 3.4v.1l-12.8.1zm10.5-2.1c0-.2 0-.4-.1-.6-.2-1.6-1.3-3-2.8-3.5-1-.4-2-.5-3.1-.3-2.1.2-3.9 1.8-4.3 3.8 0 .2-.1.4-.1.6h10.4z" />
+                        <path fill="#353A69" d="M45.4 54.6h-2.6v-.3-19.2c0-.9.7-1.7 1.7-1.8h12.8v.7c-.2.9-1 1.6-2 1.7h-9.6v7.4h10.8c.1.3.1.5 0 .8-.2.9-.9 1.5-1.8 1.6h-8.9v4.4l-.4 4.7z" />
+                        <path fill="#353A69" d="M86.5 41.7c.1-.1.2-.2.2-.4.6-.9 1.5-1.5 2.6-1.8 1.7-.5 3.6-.2 5 .8.1.1.2.1.3.2l.1.1-1.3 1.9c-.4-.2-.7-.4-1-.5-2.2-.9-4.8.1-5.7 2.4-.2.6-.3 1.1-.3 1.7v8.4c-.8.1-1.6.1-2.5 0v-.2-12.8c0-1 .7-1.8 1.7-1.9h.6l.3 2.1z" />
+                        <path fill="#353A69" d="M62.5 33.4v21.3h-2.4v-.4V44v-8.2c0-.7.3-1.3.7-1.8.5-.5 1.1-.7 1.7-.6z" />
+                    </svg>
+                </div>
+
+                <div className="mt-12 text-bold text-center text-gray-900 text-2xl">XRPL - Flare snapshot claim lookup</div>
+                <div className="mt-1 text-center text-gray-600">Details of the XRPL snapshot and corresponding calculation of the Spark claim ratio (1.0073) <a href="https://blog.flare.xyz/the-xrp-flr-ratio/" className="underline">can be found here</a>.</div>
+
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                    <div className="bg-white py-8 px-4 shadow rounded-lg sm:px-10">
+                        <form className="space-y-6" onSubmit={this.handleSubmit}>
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                    XRPL Address
+                                </label>
+                                <div className="mt-1">
+                                    <input id="xrpl-address" name="xrpl-address" placeholder="rTooLkitCksh5mQa67eaa2JaWHDBnHkpy" value={this.state.xrplAddress} onChange={this.handleAddressChange} type="text" required className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                                    Lookup Address
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="mt-10 mx-auto max-w-2xl">
+                    { addressDetails }
+                </div>
+
+            </div>
+        );
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    purge: [
+        './dist/**/*.html',
+        './dist/**/*.js',
+    ],
+    theme: {
+        fontFamily: {
+            sans: [
+                'Inter var',
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                '"Noto Sans"',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+                '"Noto Color Emoji"',
+            ],
+            serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+            mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+        },
+    },
+  plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+        require('autoprefixer'),
+    ]
+}
